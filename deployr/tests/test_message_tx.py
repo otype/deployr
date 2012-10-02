ffcbb9f1d9dcf9c4ab9c9b769889b88f02080ae0
Hans-Gunther Schmidt <hans@otype.de>
2012-10-03 00:30:50 +0200

    adding new test for message_tx

---
+++ deployr/tests/test_message_tx.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+
+    deployr
+
+    by hgschmidt
+
+    Copyright (c) 2012 apitrary
+
+"""
+from nose.tools.nontrivial import with_setup
+import pika
+from pika.adapters.blocking_connection import BlockingConnection
+from messagequeue.message_tx import enqueue_message, send_message
+from ostools import OS_SUCCESS
+
+
+def setup_func():
+    parameters = pika.ConnectionParameters()
+
+    global connection
+    connection = BlockingConnection(parameters)
+    assert connection
+
+    global channel
+    channel = connection.channel()
+    assert channel
+
+
+def teardown_func():
+    connection.close()
+
+
+@with_setup(setup_func, teardown_func)
+def test_enqueue_message():
+    message = { "testing": "stuff" }
+    assert OS_SUCCESS == enqueue_message(channel=channel, queue_message=message)
+
+
+@with_setup(setup_func, teardown_func)
+def test_send_message():
+    message = { "testing": "stuff" }
+    assert OS_SUCCESS == send_message(queue_message=message)
