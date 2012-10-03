166e4bd1f6d2dd7e7b9d17eedbfae4e6f7472567
Hans-Gunther Schmidt <hans@otype.de>
2012-10-03 23:29:34 +0200

    adding various constants

--- deployr/config/general_settings.py
+++ deployr/config/general_settings.py
@@ -6,15 +6,21 @@
     Copyright (c) 2012 apitrary
 
 """
+__name__ = 'deployr'
+__version__ = '0.1'
+__author__ = 'Hans-Gunther Schmidt'
+__company__ = 'apitrary'
 
 #
 # Application details
 #
 APP_DETAILS = {
-    'name': 'deployr',
-    'version': '0.1',
-    'company': 'apitrary',
+    'name': __name__,
+    'version': __version__,
+    'company': __company__,
     'support': 'http://apitrary.com/support',
     'contact': 'support@apitrary.com',
     'copyright': '2012 apitrary.com'
 }
+
+
