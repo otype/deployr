46122a7bb2d5a9e51c289a7dd64e3dd9ee731684
Hans-Gunther Schmidt <hans@otype.de>
2012-10-03 19:12:49 +0200

    switching to topic exchange in consumer

--- deployr/messagequeue/message_rx.py
+++ deployr/messagequeue/message_rx.py
@@ -11,7 +11,11 @@ import pika
 from pika import log
 from pika.adapters.select_connection import SelectConnection
 from task.task_execution import run_task
+from config.queue_settings import GENAPI_DEPLOYMENT_EXCHANGE
+from config.queue_settings import DEPLOY_ROUTING_KEY
 from config.queue_settings import GENAPI_DEPLOYMENT_QUEUE
+from config.queue_settings import BROKER_HOST
+from config.queue_settings import BROKER_PORT
 
 
 #
@@ -23,6 +27,7 @@ connection = None
 # Global channel used in conjunction with the broker
 #
 channel = None
+
 
 ##############################################################################
 #
@@ -36,7 +41,6 @@ def on_connected(connection):
         Callback method when connection to broker has been
         established.
     """
-    global channel
     log.debug('Connected to Broker! Establishing channel.')
     connection.channel(on_channel_open)
 
@@ -47,13 +51,22 @@ def on_channel_open(channel_):
     """
     global channel
     channel = channel_
+
+    log.debug("Declaring exchange: {}".format(GENAPI_DEPLOYMENT_EXCHANGE))
+    channel.exchange_declare(
+        exchange=GENAPI_DEPLOYMENT_EXCHANGE,
+        type='topic',
+        durable=True,
+        auto_delete=False
+    )
+
     log.debug("Declaring queue: {}".format(GENAPI_DEPLOYMENT_QUEUE))
     channel.queue_declare(
         queue=GENAPI_DEPLOYMENT_QUEUE,
+        callback=on_queue_declared,
         durable=True,
         exclusive=False,
-        auto_delete=False,
-        callback=on_queue_declared
+        auto_delete=False
     )
 
 
@@ -62,8 +75,27 @@ def on_queue_declared(frame):
         Queue has been declared. Now start to consume messages
         from the queue ...
     """
-    log.debug("Consuming message from queue \'{}\'".format(GENAPI_DEPLOYMENT_QUEUE))
-    channel.basic_consume(handle_delivery, queue=GENAPI_DEPLOYMENT_QUEUE)
+    log.debug("Consuming message from exchange=\'{}\' running on queue=\'{}\'".format(
+        GENAPI_DEPLOYMENT_EXCHANGE, GENAPI_DEPLOYMENT_QUEUE)
+    )
+    log.info('Frame: {}'.format(frame))
+
+    log.debug('Binding to queue: {}'.format(GENAPI_DEPLOYMENT_QUEUE))
+    channel.queue_bind(
+        callback=on_queue_bound,
+        exchange=GENAPI_DEPLOYMENT_EXCHANGE,
+        queue=GENAPI_DEPLOYMENT_QUEUE,
+        routing_key=DEPLOY_ROUTING_KEY
+    )
+
+
+def on_queue_bound(frame):
+    """
+        Queue is bound! Now start consuming!
+    """
+    log.debug('Queue \'{}\' bound!'.format(GENAPI_DEPLOYMENT_QUEUE))
+    log.debug('Frame: {}'.format(frame))
+    channel.basic_consume(consumer_callback=handle_delivery, queue=GENAPI_DEPLOYMENT_QUEUE)
 
 
 def handle_delivery(channel, method_frame, header_frame, body):
@@ -76,6 +108,7 @@ def handle_delivery(channel, method_frame, header_frame, body):
         method_frame.delivery_tag,
         body
     )
+    log.debug('Acknowledging received message.')
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
     # Run the task from parsed message
@@ -89,7 +122,7 @@ def handle_delivery(channel, method_frame, header_frame, body):
 ##############################################################################
 
 
-def start_consumer(broker_host='127.0.0.1', broker_port=5672):
+def start_consumer(broker_host=BROKER_HOST, broker_port=BROKER_PORT):
     """
         Start the consumer IOLoop
     """
