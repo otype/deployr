bb04a6ab30359e9239a56adb91b98aef78119e16
Hans-Gunther Schmidt <hans@otype.de>
2012-10-03 22:44:06 +0200

    adding test for supervisor_xml_rpc actions

---
+++ deployr/tests/test_supervisor_xml_rpc_api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+
+    <application_name>
+
+    by hgschmidt
+
+    Copyright (c) 2012 apitrary
+
+"""
+from ostools import OS_SUCCESS
+from supervisor.supervisor_xml_rpc_api import supervisor_xmlrpc_reload_config
+
+def setup_func():
+    "set up test fixtures"
+
+
+def teardown_func():
+    "tear down test fixtures"
+
+
+def test_supervisor_xmlrpc_reload_config():
+    assert OS_SUCCESS == supervisor_xmlrpc_reload_config()
