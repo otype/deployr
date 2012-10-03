1f5c88613af96f971b109d0f6b6336e3f255f4ff
Hans-Gunther Schmidt <hans@otype.de>
2012-10-03 22:41:39 +0200

    adding 'which' function

--- deployr/ostools/path_finders.py
+++ deployr/ostools/path_finders.py
@@ -7,6 +7,7 @@
 
 """
 import sys
+import os
 
 
 def python_interpreter_path():
@@ -14,3 +15,23 @@ def python_interpreter_path():
         Get the full path to the Python interpreter used here in deployr
     """
     return sys.executable
+
+
+def which(program):
+    """
+        Works like shell's 'which'
+    """
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
